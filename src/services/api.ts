668411559f8944f33a55f7fa510ec2043c01058a
gacneves <gabriel.angelo1008@gmail.com>
2022-10-30 21:09:28 -0300

    Created api request for random words

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import { REACT_APP_RAPID_API_KEY } from '@env'
+
+// Fetchs multiple random secret words from the API
+const fetchSecretWords = async () => {
+  console.log('fetchSecretWords');
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+    }
+  };
+
+  const response = await fetch('https://random-words5.p.rapidapi.com/getMultipleRandom?count=5&wordLength=5', options)
+  const data = await response.json();
+  return data;
+};
+
+export default fetchSecretWords;
